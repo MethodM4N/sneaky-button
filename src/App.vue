12bcadcdbf747a0c0539436a55c0d093d6aab76e
Alexandr Kolganov <gengizar@yandex.ru>
2024-10-25 20:15:56 +0500

    feat(): change favicon, add profile link

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
-<script setup></script>
+<script setup>
+import GithubIcon from '@/components/GithubIcon/GithubIcon.vue';
+</script>
 
 <template>
+  <a class="fixed right-3 bottom-3 z-[9999]" href="https://github.com/MethodM4N" target="_blank">
+    <GithubIcon
+      class="h-12 w-12 hover:scale-110 ease-in-out duration-150 rounded-full shadow-md max-sm:h-9 max-sm:w-9" />
+  </a>
   <router-view></router-view>
 </template>
